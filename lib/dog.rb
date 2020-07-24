2a4b5c252a8dc53de696bc12c72d3d44ef955c64
john beltrez <beltrezjohn@gmail.com>
2020-07-24 18:02:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,4 @@
-
-
 class Dog 
   def name=(name)
     @this_dogs_name=name 
   end 
-  def name
-    @this_dogs_name
-  end 
-  
-  def breed=(dogs_breed)
-    @this_dogs_breed=dogs_breed 
-  end 
-  def breed 
-    @this_dogs_breed
-  end 
-end 
